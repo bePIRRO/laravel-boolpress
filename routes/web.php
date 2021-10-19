d12de9a718e69c110ced831f825961bcbfc4b506
bePIRRO <manuel.pirro@gmail.com>
2021-10-19 18:55:48 +0200

    fix routes + admin controller

--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get("/", function () {
-    return view("guest.home");
+Route::get('/', function () {
+    return view('guest.home');
 });
 
-Auth::routes(["register" => false]);
+Auth::routes(['register' => false]);
 
 
-Route::middleware("auth")->name("admin.")->prefix("admin")->group(function() {
-    Route::get("/", "Admin\HomeController@index")->name("home");
+Route::middleware('auth')->name('admin')->prefix('admin')->namespace('Admin')->group(function() {
+    Route::get('/', 'HomeController@index')->name('.home');
 });
