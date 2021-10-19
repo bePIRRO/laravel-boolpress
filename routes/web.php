0c3c71253e6a12feb4f6b2a73f944f194454d5da
bePIRRO <manuel.pirro@gmail.com>
2021-10-19 18:26:37 +0200

    change routes + admin/guest folders

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get("/", function () {
+    return view("guest.home");
 });
 
-Auth::routes();
+Auth::routes(["register" => false]);
 
-Route::get('/home', 'HomeController@index')->name('home');
+
+Route::middleware("auth")->name("admin.")->prefix("admin")->group(function() {
+    Route::get("/", "Admin\HomeController@index")->name("home");
+});
