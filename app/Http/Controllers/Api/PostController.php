a38a198f89c81c08aed4b7a2347340d0931cba96
bePIRRO <manuel.pirro@gmail.com>
2021-10-24 18:00:22 +0200

    ordered desc posts

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -15,8 +15,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        $posts = Post::paginate(5);
-
+        $posts = Post::orderBy("id", "desc")->paginate(5);
         return response()->json($posts);
     }
 
